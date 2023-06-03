a29cd5bc33afa7cc57d17148381c90620d71d16c
Jaskeerat Singh <jaskeerat.singh@bizongo.com>
2023-06-03 12:06:13 +0530

    feat: main Initialised a new next app with tailwind css

---
+++ application/pages/index.js
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+   <div className='text-3xl font-bold underline bg-red-200'>Hello new project</div>
+  )
+}
